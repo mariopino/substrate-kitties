6804c4aa44baecf63bea1bf5fcfba3a043877d15
mariopino <mariopino@protonmail.com>
2021-05-18 15:30:30 +0200

    ensure kitty id does not overflow

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -2,7 +2,7 @@
 
 use codec::{Encode, Decode};
 use frame_support::{
-	decl_module, decl_storage, decl_event, StorageValue, StorageDoubleMap,
+	decl_module, decl_storage, decl_event, decl_error, StorageValue, StorageDoubleMap,
 	traits::Randomness, RuntimeDebug
 };
 use sp_io::hashing::blake2_128;
@@ -15,6 +15,15 @@ pub trait Trait: frame_system::Trait {
 	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
 }
 
+decl_storage! {
+	trait Store for Module<T: Trait> as Kitties {
+		/// Stores all the kitties, key is the kitty id
+		pub Kitties get(fn kitties): double_map hasher(blake2_128_concat) T::AccountId, hasher(blake2_128_concat) u32 => Option<Kitty>;
+		/// Stores the next kitty id
+		pub NextKittyId get(fn next_kitty_id): u32;
+	}
+}
+
 decl_event! {
 	pub enum Event<T> where
 		<T as frame_system::Trait>::AccountId,
@@ -24,12 +33,10 @@ decl_event! {
 	}
 }
 
-decl_storage! {
-	trait Store for Module<T: Trait> as Kitties {
-		/// Stores all the kitties, key is the kitty id
-		pub Kitties get(fn kitties): double_map hasher(blake2_128_concat) T::AccountId, hasher(blake2_128_concat) u32 => Option<Kitty>;
-		/// Stores the next kitty id
-		pub NextKittyId get(fn next_kitty_id): u32;
+decl_error! {
+	pub enum Error for Module<T: Trait> {
+		/// A kitty is created, \[owner, kitty_id, kitty\]
+		KittiesIdOverflow,
 	}
 }
 
@@ -40,6 +47,18 @@ decl_module! {
 		#[weight = 1000]
 		pub fn create(origin) {
 			let sender = ensure_signed(origin)?;
+
+			// ensure kitty id does not overflow
+			let kitty_id = Self::next_kitty_id();
+			match kitty_id.checked_add(1) {
+				Some(_) => {
+					// will not overflow
+				}
+				None => {
+					// Overflow!
+					return Err(Error::<T>::KittiesIdOverflow.into());
+				}
+			};
 
 			// Generate a random 128bit value
 			let payload = (
